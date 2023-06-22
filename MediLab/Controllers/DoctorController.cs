a6fd698c8363654fbf9e2a5e2b50fb84d22d1a79
AliyevMura <7lvghl9@code.edu.az>
2023-06-22 15:07:11 +0400

    Initial commit

---
+++ MediLab/Controllers/DoctorController.cs
@@ -0,0 +1,34 @@
+﻿using MediLab.DAL;
+using MediLab.Models;
+using MediLab.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediLab.Controllers
+{
+	public class DoctorController : Controller
+	{
+        private readonly AppDbContext _context;
+
+        public DoctorController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            DoctorVM doctorVM = new DoctorVM()
+            {
+                Services = await _context.Types
+                .Include(s=>s.Doctors).ToListAsync(),
+                Doctors = await _context.Doctors
+                .Include(d => d.TypeOfService)
+                .OrderByDescending(d => d.Id)
+                .ToListAsync()
+
+            };
+            return View(doctorVM);
+            //return View(await _context.Doctors.Where(p => !p.IsDeleted).OrderByDescending(p => p.Id).ToListAsync());
+
+        }
+    }
+}
---
+++ MediLab/Models/TypeOfService.cs
@@ -0,0 +1,18 @@
+﻿namespace MediLab.Models;
+
+public class TypeOfService
+{
+    public TypeOfService()
+    {
+        Doctors = new List<Doctor>();
+    }
+    public int Id { get; set; }
+	public string Name { get; set; }
+	public virtual List<Doctor> Doctors { get; set; }
+	public bool IsDeleted { get; set; } = default;
+
+
+
+
+
+}
